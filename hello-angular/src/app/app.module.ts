d4adcaa8d6a2c3ade8f7f493508c451304646548
richey.liu <richey.liu@cnbexpress.com>
2017-03-29 18:47:57 +0800

    update

--- hello-angular/src/app/app.module.ts
+++ hello-angular/src/app/app.module.ts
@@ -2,27 +2,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { LoginComponent } from './login/login.component';
-
+import {LoginComponent} from './login/login.component';
+import {TodoModule} from './todo/todo.module'
 import {AppComponent} from './app.component';
 import {AuthService} from './core/auth.service';
 import {routing} from './app.routes';
-import { TodoComponent } from './todo/todo.component';
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryTodoDbService } from './todo/todo-data';
+// import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+// import {InMemoryTodoDbService} from './todo/todo-data';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    TodoComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryTodoDbService),
-    routing
+    // InMemoryWebApiModule.forRoot(InMemoryTodoDbService),
+    routing,
+    TodoModule
   ],
   providers: [
     {provide: 'auth', useClass: AuthService}
