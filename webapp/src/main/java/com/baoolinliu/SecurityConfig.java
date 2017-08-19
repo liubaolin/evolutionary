609f89e855925609838e78104242f8c922a44d01
richey <richey@192.168.5.21>
2017-08-19 23:56:12 +0800

    update

--- webapp/src/main/java/com/baoolinliu/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.baoolinliu;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * Created by richey on 17-3-12.
- */
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-}
