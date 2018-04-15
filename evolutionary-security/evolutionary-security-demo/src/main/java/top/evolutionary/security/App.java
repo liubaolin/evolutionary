319082d95a023f8dcfef313e34808ed4efdc16d3
richey <richey@192.168.5.4>
2018-04-15 18:00:47 +0800

    Spring-security 项目结构初始化

---
+++ evolutionary-security/evolutionary-security-demo/src/main/java/top/evolutionary/security/App.java
@@ -0,0 +1,13 @@
+package top.evolutionary.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
