69585cb1d612105613c4a3128f62c7352baf5487
richey <richey@192.168.5.14>
2018-04-30 15:17:35 +0800

    用户认证

---
+++ evolutionary-security/evolutionary-security-browser/src/main/java/top/evolutionary/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,29 @@
+package top.evolutionary.security.browser;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //        http.httpBasic();  http Basic的认证方式
+        http.formLogin()  //表单登录
+                .and()
+                .authorizeRequests() //所有请求都需要认证
+                .anyRequest()
+                .authenticated();
+
+    }
+}
